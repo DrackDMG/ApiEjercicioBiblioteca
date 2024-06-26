136703111eb2b7d6ad4124ba98ad39d6fb33910f
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-06-26 10:04:08 -0600

    Creacion delos datos de prueba

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+INSERT INTO Author (name, country, birth_date) VALUES ('Juan', 'México', '1990-01-01');
+INSERT INTO Author (name, country, birth_date) VALUES ('Ana', 'México', '1991-02-02');
+INSERT INTO Author (name, country, birth_date) VALUES ('Carlos', 'México', '1992-03-03');
+INSERT INTO Author (name, country, birth_date) VALUES ('María', 'México', '1993-04-04');
+INSERT INTO Author (name, country, birth_date) VALUES ('Pedro', 'México', '1994-05-05');
+
+
+
+
+
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 1', '0001', 1, '2000-01-01');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 2', '0002', 2, '2001-02-02');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 3', '0003', 3, '2002-03-03');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 4', '0004', 4, '2003-04-04');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 5', '0005', 5, '2004-05-05');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 6', '0006', 1, '2005-06-06');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 7', '0007', 2, '2006-07-07');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 8', '0008', 3, '2007-08-08');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 9', '0009', 4, '2008-09-09');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 10', '0010', 5, '2009-10-10');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 11', '0011', 1, '2010-11-11');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 12', '0012', 2, '2011-12-12');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 13', '0013', 3, '2012-01-13');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 14', '0014', 4, '2013-02-14');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 15', '0015', 5, '2014-03-15');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 16', '0016', 1, '2015-04-16');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 17', '0017', 2, '2016-05-17');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 18', '0018', 3, '2017-06-18');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 19', '0019', 4, '2018-07-19');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 20', '0020', 5, '2019-08-20');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 21', '0021', 1, '2020-09-21');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 22', '0022', 2, '2021-10-22');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 23', '0023', 3, '2022-11-23');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 24', '0024', 4, '2023-12-24');
+INSERT INTO Book (title, code, author_id, published_date) VALUES ('Libro 25', '0025', 5, '2024-01-25');
