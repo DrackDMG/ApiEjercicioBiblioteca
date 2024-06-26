c25baa21ac49a5abfc62d7f9fc3192cb7e6dd517
dmoralgo <dmoralgo@EMEAL.NTTDATA.COM>
2024-06-26 09:39:58 -0600

    commit inical y configuracion del .properties

---
+++ src/main/java/com/drack/BibliotecaEjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.drack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaEjercicioApplication.class, args);
+	}
+
+}
